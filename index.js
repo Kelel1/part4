127d91c3fdf0c6154ca05f1f640b39c6f6ee0298
Kern Elder <kelel1@yahoo.com>
2019-07-27 17:43:25 -0400

    initial blog buildout

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("Hello Kern")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+let blogs = [{id: 1, title: "Kel", author: "Elder", url: "https/fes", likes: 45}]
+
+// Middlewares
+app.use(bodyParser.json())
+app.use(cors())
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+    response.send('<h1>Welcome to Blog-List</h1>')
+})
+
+app.get('/blogs', (request, response) => {
+    response.json(blogs)
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
