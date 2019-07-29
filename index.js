799ccfdbae4026ec488bb6506cee2f78b41f8fcd
Kern Elder <kelel1@yahoo.com>
2019-07-29 19:47:03 -0400

    update index.js & add app.js

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-// site on heroku begins w/ floating
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const app = require('./app')
+const http = require('http') 
+const config = require('./utils/config')
 
-const Blog = mongoose.model('Blog', blogSchema)
+const server = http.createServer(app) 
 
-const mongoUrl = 
-    'mongodb+srv://Kelder:ZF6kemCTS28tYAyn@cluster1-xo9e5.mongodb.net/phonebook-app?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/', (request, response) => {
-    response.send('<h1>Welcome to Blog List</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
